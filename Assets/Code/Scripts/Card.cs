777fe049e1f05da8feda9550b7655cc8d9b0b9bd
Carlos Ruminott <carlosruminott@gmail.com>
2022-05-15 03:54:19 -0300

    re ajuste info carta, ocultamiento de campos cuando no es character

--- Assets/Code/Scripts/Card.cs
+++ Assets/Code/Scripts/Card.cs
@@ -9,6 +9,7 @@ public class Card : MonoBehaviour
     [SerializeField] private BaseCard _cardInfo;
     [SerializeField] private SpriteRenderer _image;
     [SerializeField] private TextMeshPro _name, _description, _hp, _defense, _attack, _elementalType, _hability;
+    [SerializeField] private GameObject _characterFields;
 
     public BaseCard CardInfo
     {
@@ -30,10 +31,16 @@ public class Card : MonoBehaviour
         _image.sprite = _cardInfo.cardImage;
         _name.text = _cardInfo.cardName;
         _description.text = _cardInfo.desciption;
-        _hp.text = "HP: "+_cardInfo.healthPoints.ToString();
-        _defense.text = "Def: "+_cardInfo.defense.ToString();
-        _attack.text = "Att: "+_cardInfo.attack.ToString();
-        _elementalType.text = "Type: "+_cardInfo.elementalType.ToString();
-        //_hability.text = _cardInfo.habilityDesciption;
+
+        if (_cardInfo.cardType.ToString() == "Character")
+        {
+            _characterFields.SetActive(true);
+            _hp.text = "HP: " + _cardInfo.healthPoints.ToString();
+            _defense.text = "Def: " + _cardInfo.defense.ToString();
+            _attack.text = "Att: " + _cardInfo.attack.ToString();
+            _elementalType.text = "Type: " + _cardInfo.elementalType.ToString();
+            //_hability.text = _cardInfo.habilityDesciption;
+        }
+
     }
 }
--- Assets/Code/Scripts/Deck.cs
+++ Assets/Code/Scripts/Deck.cs
@@ -6,7 +6,7 @@ public class Deck : MonoBehaviour
 {
     [SerializeField] private List<BaseCard> _cardList;
     [SerializeField] private GameObject _card;
-    [SerializeField] private int _initialNumHandCards = 1;
+    [SerializeField] private int _initialNumHandCards = 2;
     [SerializeField] private float _positionFirstCard = -5f;
     [SerializeField] private float _distanceOtherCard = 3.5f;
     private BaseCard _tempBC;
@@ -21,7 +21,7 @@ public class Deck : MonoBehaviour
     private void GetHand(Card card)
     {
         float posX = _positionFirstCard;
-        for (int i = 0; i < _initialNumHandCards; i++)
+        for (int i = 0; i <= _initialNumHandCards; i++)
         {
             card.CardInfo = _cardList[i];
             Instantiate(_card, new Vector3(posX,0,0), Quaternion.identity);

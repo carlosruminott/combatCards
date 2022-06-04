645ef0e9e88e037417ee27634e218b483179ec4c
Carlos Ruminott <carlosruminott@gmail.com>
2022-06-04 13:36:53 -0300

    WIP script de scroll view centrado cuando hay menos cartas que el componente

---
+++ Assets/Code/Components/PlayerHandView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Components
+{
+    public class PlayerHandView : MonoBehaviour
+    {
+        // cuando haya menos cartas que el contenedor de scrollview
+        // desactivar el componente scroll rect del game object scroll view
+        // y el en game object Content activar el child force expand
+    }
+}

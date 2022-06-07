31cb84dd82ee71665702b12ea40afcd93f4ae13e
Carlos Ruminott <carlosruminott@gmail.com>
2022-06-06 21:26:24 -0300

    animacion de fade y escala para el zoom de la carta

--- Assets/Code/Components/CardZoomView.cs
+++ Assets/Code/Components/CardZoomView.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Game.Logic;
+using DG.Tweening;
+using UnityEngine.UI;
 
 namespace Game.Components
 {
@@ -9,13 +11,18 @@ namespace Game.Components
     {
         //[SerializeField] private GameObject _card;
 
+        [SerializeField] CanvasGroup _canvasGroup;
+
         GameObject _childCard;
         Card _cardScript;
 
         private void Awake()
         {
+            Debug.Assert(_canvasGroup != null, "añadir canvas group al card zoom view");
             _childCard = gameObject.transform.GetChild(0).gameObject;
             _cardScript = _childCard.GetComponent<Card>();
+            _canvasGroup.DOFade(0,0);
+            _childCard.transform.DOScale(0.95f, 0);
             _childCard.SetActive(false);
             EventDispatcher.ZoomIn.AddListener(ShowCard);
             EventDispatcher.ZoomOut.AddListener(RemoveCard);
@@ -25,6 +32,10 @@ namespace Game.Components
         {
             //Debug.Log(info.cardName);
             _childCard.SetActive(true);
+            _canvasGroup.DOKill();
+            _childCard.transform.DOKill();
+            _canvasGroup.DOFade(1, 0.3f);
+            _childCard.transform.DOScale(1f, 0.3f);
             _cardScript.CardInfo = info;
             _cardScript.InitInfo();
             if (_cardScript.CardInfo.cardType.ToString() != "Character")
@@ -36,7 +47,11 @@ namespace Game.Components
 
         private void RemoveCard()
         {
-            _childCard.SetActive(false);
+            _childCard.transform.DOScale(0.95f, 0.5f);
+            _canvasGroup.DOFade(0, 0.2f).OnComplete(()=>
+            {
+                _childCard.SetActive(false);
+            });
         }
     }
 }

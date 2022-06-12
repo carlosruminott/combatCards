3d04c8a4b0fe53ccb5540d7973407f579951f0f5
Carlos Ruminott <carlosruminott@gmail.com>
2022-06-12 02:45:55 -0300

    desactivar texto boton al instaciar carta en tile

--- Assets/Code/Components/FieldGrid/Tile.cs
+++ Assets/Code/Components/FieldGrid/Tile.cs
@@ -11,6 +11,7 @@ namespace Game.Components.Grid
         [SerializeField] private TextMeshProUGUI _numberTile;
         [SerializeField] private GameObject _tileCard;
         [SerializeField] private GameObject _tileCardButton;
+        [SerializeField] private GameObject _tileCardButtonText;
         [SerializeField] private bool _isTileActive = false;
 
         public bool isTileActive 
@@ -30,6 +31,7 @@ namespace Game.Components.Grid
             Debug.Assert(_numberTile != null, "missing text component on [" + gameObject.name + "]");
             Debug.Assert(_tileCard != null, "missing text component on [" + gameObject.name + "]");
             Debug.Assert(_tileCardButton != null, "missing text component on [" + gameObject.name + "]");
+            Debug.Assert(_tileCardButtonText != null, "missing text component on [" + gameObject.name + "]");
             _numberTile.text = transform.name;
             _tileCardScript = _tileCard.GetComponent<TileCardView>();
         }
@@ -43,6 +45,7 @@ namespace Game.Components.Grid
         {
             _isTileActive = true;
             Instantiate(_tileCard, _tileCardButton.transform);
+            _tileCardButtonText.SetActive(false);
             EventDispatcher.TileSapwned?.Invoke();
         }
     }

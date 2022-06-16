cd37b875e4d5967b2681357efb542bbe0bf41d41
Carlos Ruminott <carlosruminott@gmail.com>
2022-06-15 21:30:32 -0300

    WIP refactorización de deck

--- Assets/Code/ScriptablesObjects/DeckList.cs
+++ Assets/Code/ScriptablesObjects/DeckList.cs
@@ -7,8 +7,20 @@ namespace Game
     [CreateAssetMenu(fileName = "Deck", menuName = "Game Data/New Deck", order = 3)]
     public class DeckList : ScriptableObject
     {
+
         [Header("Scriptable Card List")]
         public List<BaseCard> cardList;
+
+        //public Dictionary<BaseCard, Image>
+
+        public List<Cards> cards;
+
+        [System.Serializable]
+        public struct Cards
+        {
+            public BaseCard card;
+            [Min(0)] public int count;
+        }
 
     }
 }

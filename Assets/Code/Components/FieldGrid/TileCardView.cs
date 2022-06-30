6752d8d8aaac39cf97a0b7c213be0043303b7dca
Carlos Ruminott <carlosruminott@gmail.com>
2022-06-29 21:02:13 -0300

    Mostrar HP en tile

--- Assets/Code/Components/FieldGrid/TileCardView.cs
+++ Assets/Code/Components/FieldGrid/TileCardView.cs
@@ -11,11 +11,14 @@ namespace Game.Components.Grid
         public BaseCard cardInfo;
 
         [SerializeField] private TextMeshProUGUI _tileCardName;
+        [SerializeField] private TextMeshProUGUI _hp;
 
         private void Start()
         {
             Debug.Assert(_tileCardName != null, "missing text component on [" + gameObject.name + "]");
+            Debug.Assert(_hp != null, "missing text component on [" + gameObject.name + "]");
             _tileCardName.text = tileCardName;
+            _hp.text = cardInfo.healthPoints.ToString();
         }
     }
 }

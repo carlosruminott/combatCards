94f32a3401a368c226967eba39314eb0cbbff312
Carlos Ruminott <carlosruminott@gmail.com>
2022-07-03 10:43:03 -0300

    metodo para mostrar stats de carta en tile

--- Assets/Code/Components/FieldGrid/TileCardView.cs
+++ Assets/Code/Components/FieldGrid/TileCardView.cs
@@ -11,14 +11,37 @@ namespace Game.Components.Grid
         public BaseCard cardInfo;
 
         [SerializeField] private TextMeshProUGUI _tileCardName;
-        [SerializeField] private TextMeshProUGUI _hp;
+        [SerializeField] private TextMeshProUGUI _hp, _atk, _def;
+
+        [SerializeField] private int _healthPoint;
+        [SerializeField] private int _attack;
+        [SerializeField] private int _defense;
 
         private void Start()
         {
             Debug.Assert(_tileCardName != null, "missing text component on [" + gameObject.name + "]");
             Debug.Assert(_hp != null, "missing text component on [" + gameObject.name + "]");
+            Debug.Assert(_atk != null, "missing text component on [" + gameObject.name + "]");
+            Debug.Assert(_def != null, "missing text component on [" + gameObject.name + "]");
+            InitInfo();
+        }
+
+        private void Update() {
+            DisplayInfo();
+        }
+
+        private void InitInfo() {
             _tileCardName.text = tileCardName;
-            _hp.text = cardInfo.healthPoints.ToString();
+            _healthPoint = cardInfo.healthPoints;
+            _attack = cardInfo.attack;
+            _defense = cardInfo.defense;
+            DisplayInfo();
+        }
+
+        private void DisplayInfo() {
+            _hp.text = _healthPoint.ToString();
+            _atk.text = _attack.ToString();
+            _def.text = _defense.ToString();
         }
     }
 }

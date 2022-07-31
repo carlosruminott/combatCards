d8903b72af9e39cd787e33210301f24107041a5b
Carlos Ruminott <carlosruminott@gmail.com>
2022-07-31 11:29:17 -0300

    WIP habilidad poción

--- Assets/Code/Components/Abilities/Potion.cs
+++ Assets/Code/Components/Abilities/Potion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Game.Logic;
 using Game.Components.Grid;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.Components.Abilities {
     public class Potion : Ability
@@ -17,8 +18,19 @@ namespace Game.Components.Abilities {
             Debug.Log("Cura 2 puntos de 1 jugador");
             _active = true;
             EventDispatcher.PlayCardToField?.Invoke();
+
             //TODO:
             //seleccionar carta en campo y restaurarle 2 puntos de vida
+
+            //hay que obtener los tile buttons de los gameobjects de la grilla del 30 al 35 o que estén activos
+            
+
+
+            // y agregarle el listener
+            GetComponentInParent<Button>().onClick.AddListener(() => {
+                    var goButton = transform.gameObject;
+                    Heal(goButton);
+                });
         }
 
         public void Heal(GameObject go) {
@@ -28,6 +40,7 @@ namespace Game.Components.Abilities {
                 tileCardController.Heal(_healPoints);
                 _active = false;
                 WasPlayed = true;
+                //TODO: habria que eliminar el listener
             }
         }
     }
---
+++ Assets/Code/Components/FieldGrid/TileButtonHability.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Logic;
+using UnityEngine.UI;
+
+namespace Game.Components.Grid
+{
+    public class TileButtonHability : MonoBehaviour
+    {
+        // private void Awake() {
+        //     EventDispatcher.TileSapwned.AddListener(GetHability);
+        // }
+
+        // private void GetHability() {
+        //     TileCardView _tileCardScript = transform.GetChild(1).gameObject.GetComponent<TileCardView>();
+        //     var abilityName = _tileCardScript.CardInfo.abilityName;
+        //     Debug.Log("esta es la habilidad "+abilityName);
+        // }
+    }
+}
